3162edc12f975e5a1c13ebd230c35336e7c38fdf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-03-11 19:01:36 +1100

    Use Prettier to auto-format code and check it with eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
 const NEWLINE = '\n'
 const MULTILINE_PATTERN = new RegExp(`^${NEWLINE}( *)`)
 
-function multiline(text) {
+function multiline (text) {
   const match = text.match(MULTILINE_PATTERN)
   const skipLength = match[1].length
-  return text.split(NEWLINE)
+  return text
+    .split(NEWLINE)
     .slice(1)
     .map(line => line.substr(skipLength))
     .join(NEWLINE)
