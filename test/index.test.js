91ed7946898e8274904e7c3a9c41c2b5c7ff7627
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-17 13:33:55 +1000

    Removed chai from dev dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect
+const { strictEqual } = require('assert')
 const createMultiline = require('../index')
 const defaultMultiline = createMultiline()
 
@@ -6,12 +6,12 @@ describe('multiline', () => {
   it('ignores the first empty line', () => {
     const str = defaultMultiline(`
 LINE1`)
-    expect(str).to.eql('LINE1')
+    strictEqual(str, 'LINE1')
   })
 
   it('does nothing if given string does not begin with newline', () => {
     const str = defaultMultiline('LINE1')
-    expect(str).to.eql('LINE1')
+    strictEqual(str, 'LINE1')
   })
 
   it('uses the indent size of the first non-empty line', () => {
@@ -19,13 +19,13 @@ LINE1`)
 
       LINE1
       LINE2`)
-    expect(str).to.eql('\nLINE1\nLINE2')
+    strictEqual(str, '\nLINE1\nLINE2')
   })
 
   it('removes all leading space characters', () => {
     const str = defaultMultiline(`
               LINE1`)
-    expect(str).to.eql('LINE1')
+    strictEqual(str, 'LINE1')
   })
 
   it('sets the indent of the 2nd line as the indent for all lines', () => {
@@ -34,7 +34,7 @@ LINE1`)
                 - LINE2
               - LINE3
               `)
-    expect(str).to.eql('- LINE1\n  - LINE2\n- LINE3\n')
+    strictEqual(str, '- LINE1\n  - LINE2\n- LINE3\n')
   })
 
   it('recognises tabs as an indent', () => {
@@ -43,21 +43,21 @@ LINE1`)
 		- LINE1
 		- LINE2`)
     /* eslint-enable no-tabs */
-    expect(str).to.eql('- LINE1\n- LINE2')
+    strictEqual(str, '- LINE1\n- LINE2')
   })
 
   it('removes indents only if it is the same with the first indent', () => {
     const str = defaultMultiline(`
         - LINE1
       x - LINE2`)
-    expect(str).to.eql('- LINE1\n      x - LINE2')
+    strictEqual(str, '- LINE1\n      x - LINE2')
   })
 
   it('does not remove matching spaces if they appear in the middle of a line', () => {
     const str = defaultMultiline(`
         - LINE1
       - LINE2        x`)
-    expect(str).to.eql('- LINE1\n      - LINE2        x')
+    strictEqual(str, '- LINE1\n      - LINE2        x')
   })
 
   it('drops all characters until it encounters the first margin character', () => {
@@ -66,7 +66,7 @@ LINE1`)
         |  *
         | ***
         |*****`)
-    expect(str).to.eql('  *\n ***\n*****')
+    strictEqual(str, '  *\n ***\n*****')
   })
 
   it('does nothing if it encounters non-margin-mark or non-spaces first', () => {
@@ -74,6 +74,6 @@ LINE1`)
     const str = multiline(`
       x |  *
         | ***`)
-    expect(str).to.eql('      x |  *\n        | ***')
+    strictEqual(str, '      x |  *\n        | ***')
   })
 })
